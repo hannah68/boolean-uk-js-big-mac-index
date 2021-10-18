abf5bf617aed1406e1a2757f67b3a0eef9ff30ec
hannah68 <reihaneh.naderi1989@gmail.com>
2021-10-18 17:22:27 +0100

    update js file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let UsBigMac = 5.65;
-let UkBigMac = 3.49;
-let SwissBigMac = 12.900;
-let SouthAfricaBigMac = 33.50;
-let FranceBigMac = 5.16;
-let SwedenBigMac = 52;
+const UsBigMac = 5.65;
+const UkBigMac = 3.49;
+const SwissBigMac = 12.900;
+const SouthAfricaBigMac = 33.50;
+const FranceBigMac = 5.16;
+const SwedenBigMac = 52;
 
 let UkIpp = UsBigMac / UkBigMac;
 let SwissIpp = UsBigMac/ SwissBigMac;
@@ -11,11 +11,11 @@ let SouthAfricaIpp = UsBigMac / SouthAfricaBigMac;
 let FranceIpp = UsBigMac/ FranceBigMac;
 let SwedenIpp = UsBigMac/ SwedenBigMac;
 
-let UkCurr = 1.37;
-let SwissCurr = 1.08;
-let SouthAfricaCurr = 0.68;
-let FranceCurr = 1.16;
-let swedenCurr = 0.12;
+const UkCurr = 1.37;
+const SwissCurr = 1.08;
+const SouthAfricaCurr = 0.68;
+const FranceCurr = 1.16;
+const swedenCurr = 0.12;
 
 
 console.log(`UkIpp: ${UkIpp}`);
@@ -27,11 +27,59 @@ console.log(`SwedenIpp: ${SwedenIpp}`);
 
 console.log('====================');
 
-console.log(`uk Index : ${((UkCurr - UkIpp)/ UkIpp) * 100}`);
-console.log(`swiss Index : ${((SwissCurr - SwissIpp)/ SwissIpp) * 100 }`);
-console.log(`south africa Index: ${((SouthAfricaCurr - SouthAfricaIpp)/ SouthAfricaIpp) * 100}`);
-console.log(`France Index : ${((FranceCurr - FranceIpp)/ FranceIpp) * 100}`);
-console.log(`Sweden Index: ${((swedenCurr - SwedenIpp)/ SwedenIpp) * 100}`);
+let ukIndex = (((UkCurr - UkIpp)/ UkIpp) * 100).toFixed(2);
+let SwissIndex = (((SwissCurr - SwissIpp)/ SwissIpp) * 100).toFixed(2);
+let SouthAfricaIndex = (((SouthAfricaCurr - SouthAfricaIpp)/ SouthAfricaIpp) * 100).toFixed(2);
+let FranceIndex = (((FranceCurr - FranceIpp)/ FranceIpp) * 100).toFixed(2);
+let SwedenIndex = (((swedenCurr - SwedenIpp)/ SwedenIpp) * 100).toFixed(2);
+
+console.log(`uk Index : ${ukIndex}`);
+console.log(`swiss Index : ${SwissIndex}`);
+console.log(`south africa Index: ${SouthAfricaIndex}`);
+console.log(`France Index : ${FranceIndex}`);
+console.log(`Sweden Index: ${SwedenIndex}`);
+
+console.log('challenge 1 =============');
+let usIndex = 0;
+
+// compare two values
+console.log(ukIndex < usIndex);
+console.log(SwissIndex < usIndex);
+console.log(SouthAfricaIndex < usIndex);
+console.log(FranceIndex < usIndex);
+console.log(SwedenIndex < usIndex);
+
+
+if(usIndex < ukIndex){
+    console.log(`Us is undervalued`);
+}else{
+    console.log(`pound is undervalued`);
+}
+
+
+if(usIndex < SwissIndex){
+    console.log(`Us is undervalued`);
+}else{
+    console.log(`franc is undervalued`);
+}
+
+if(usIndex < SouthAfricaIndex){
+    console.log(`Us is undervalued`);
+}else{
+    console.log(`Zar is undervalued`);
+}
+
+if(usIndex < FranceIndex){
+    console.log(`Us is undervalued`);
+}else{
+    console.log(`Euro is undervalued`);
+}
+
+if(usIndex < SwedenIndex){
+    console.log(`Us is undervalued`);
+}else{
+    console.log(`Krona is undervalued`);
+}
 
 
 
